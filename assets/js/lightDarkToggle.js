b9805b3d77f5a3cb163d3524ed33fdc2e6ba0c36
akarczmarz2000 <akarczmarz2000@gmail.com>
2020-04-06 21:52:43 +0000

    Added area for dark light button

--- assets/js/lightDarkToggle.js
+++ assets/js/lightDarkToggle.js
@@ -1,10 +1,4 @@
 function myFunction() {
    var element = document.section;
-   if (element.classList.contains("content")){
-       element.classList.remove("content")
-       element.classList.add("dark-mode")
-   }else{
-       element.classList.remove("dark-mode")
-       element.classList.add("content")
-   }
+   element.classList.toggle("dark-mode");
 }
