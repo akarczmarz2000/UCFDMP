fd3963a589a6072d7b5383ea94f676be4c5aa336
akarczmarz2000 <akarczmarz2000@gmail.com>
2020-04-09 03:43:07 +0000

    Footer trying to fix

--- assets/js/footerResize.js
+++ assets/js/footerResize.js
@@ -1,15 +1,15 @@
 function footerResizeSmall(screenQuerySmall){
     if (screenQuerySmall.matches){
-        document.getElementById("resizeSmall").style.display = "none";
-    }else{
         document.getElementById("resizeSmall").style.display = "inline";
+    }else{
+        document.getElementById("resizeSmall").style.display = "none";
     }
 }
 
 function footerResizeNormal(screenQueryNormal){
     if (screenQueryNormal.matches){
-        document.getElementById("resizeNormal").style.display = "inline";
-    }else{
         document.getElementById("resizeNormal").style.display = "none";
+    }else{
+        document.getElementById("resizeNormal").style.display = "inline";
     }
 }
