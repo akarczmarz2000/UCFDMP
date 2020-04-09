ced19db7594ddb12cb1b127fbb8a985e7dfbbc60
akarczmarz2000 <akarczmarz2000@gmail.com>
2020-04-09 01:29:01 +0000

    Footer updated

--- assets/js/footerResize.js
+++
@@ -1,9 +0,0 @@
-function footerResize() {
-    var w = window.outerWidth;
-    var element = document.getElementsByClassName("footerResize");
-    if (w <= 816){
-        element.classList.remove("col-sm-4")
-    }else if (screen.width >= 820){
-        element.classList.add("col-sm-4")
-    }
-}
