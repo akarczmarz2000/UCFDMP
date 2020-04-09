3cd357e24608c9196bc862ff4193923da155a538
akarczmarz2000 <akarczmarz2000@gmail.com>
2020-04-09 02:44:28 +0000

    Footer Resize JS added

--- assets/js/footerResize.js
+++ assets/js/footerResize.js
@@ -1,17 +1,17 @@
 function footerResizeSmall(screenQuerySmall){
     var footerResize = document.getElementsByClassName(footer-resize-small)
     if (screenQuerySmall.matches){
-        footerResize.style.display = "inline";
-    }else{
         footerResize.style.display = "none";
+    }else{
+        footerResize.style.display = "inline";
     }
 }
 
 function footerResizeNormal(screenQueryNormal){
     var footerResize = document.getElementsByClassName(footer-resize-small)
     if (screenQuerySmall.matches){
-        footerResize.style.display = "inline";
-    }else{
         footerResize.style.display = "none";
+    }else{
+        footerResize.style.display = "inline";
     }
 }
