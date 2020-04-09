b8e47c27529d13582f1e6fa460d4d10a139da5b3
akarczmarz2000 <akarczmarz2000@gmail.com>
2020-04-09 01:00:22 +0000

    Footer updated

--- assets/js/footerResize.js
+++ assets/js/footerResize.js
@@ -1,6 +1,7 @@
 function footerResize() {
+    var w = window.outerWidth;
     var element = document.getElementsByClassName("footerResize");
-    if (screen.width <= 816){
+    if (w <= 816){
         element.classList.remove("col-sm-4")
     }else if (screen.width >= 820){
         element.classList.add("col-sm-4")
